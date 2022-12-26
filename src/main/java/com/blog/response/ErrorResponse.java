4c1c54ddc87029862d5c10f8af5d74418dfbdb6e
eunho.moon@gmail.com <eunho.moon@gmail.com>
2022-12-26 15:57:20 +0900

    예외 처리

--- src/main/java/com/blog/response/ErrorResponse.java
+++ src/main/java/com/blog/response/ErrorResponse.java
@@ -3,6 +3,7 @@ package com.blog.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -26,7 +27,7 @@ public class ErrorResponse {
     public ErrorResponse(String code, String message, Map<String, String> validation) {
         this.code = code;
         this.message = message;
-        this.validation = validation;
+        this.validation = validation != null ? validation : new HashMap<>();
     }
 
     public void addValidation(String fieldName, String errorMessage) {

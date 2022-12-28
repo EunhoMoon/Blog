9763b5b07eed809cf5d4893248f8795421ff2afa
eunho.moon@gmail.com <eunho.moon@gmail.com>
2022-12-28 16:43:59 +0900

    Vue 파일 정리

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const count = 0;
+</script>
+
+<template>
+  <p>안녕하세요.</p> <button @click="count += 1">버튼을 눌러주세요</button>
+  <p>{{count}}</p>
+</template>
+
+<style>
+
+</style>
